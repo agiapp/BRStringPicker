4dff50bf2604e6a1d6d3900c3d8eefcfb0dda2ab
任波 <borenfocus@gmail.com>
2017-02-22 16:50:44 +0800

    用Xcode创建项目

---
+++ BRStringPicker/BRStringPicker/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BRStringPicker
+//
+//  Created by 任波 on 17/2/22.
+//  Copyright © 2017年 renbo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ BRStringPicker/BRStringPicker/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  BRStringPicker
+//
+//  Created by 任波 on 17/2/22.
+//  Copyright © 2017年 renbo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
